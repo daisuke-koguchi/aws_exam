a6e86c71c23905699169b472c2689e230310e572
daisuke koguchi <ds05080906@gmail.com>
2022-01-05 14:10:33 +0900

    add blogappアプリを作成

---
+++ app/models/blog.rb
@@ -0,0 +1,11 @@
+class Blog < ApplicationRecord
+    # def self.latest(number)
+    #     order(created_at: :desc).limit(number)
+    # end
+    # def title_change
+    #     update(title: 'changed')
+    # end
+    #scope :latest, ->(number = 3){order(created_at: :desc).limit(number)}
+   include CommonModule
+   validates_with BlogValidator
+end
---
+++ app/models/topic.rb
@@ -0,0 +1,5 @@
+class Topic < ApplicationRecord
+    #scope :latest, -> (number = 3){order(created_at: :desc).limit(number)}
+    include CommonModule
+    test_puts
+end
